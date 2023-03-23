d046a2752c06a8e4d242f97aeeedb4ccd24d3542
AbiHanna-Guy <AbiHanna.Guy@bcg.com>
2023-03-23 15:02:57 +0100

    updates to reader

--- scripts/model_reader.py
+++ scripts/model_reader.py
@@ -10,44 +10,20 @@ model_file = "models_mps/test_or.proto"
 # Load the model into the solver
 with open(model_file, "rb") as f:
     proto = f.read()
-
     model = linear_solver_pb2.MPModelProto()
+
     model.FromString(proto)
 
     solver.LoadModelFromProto(model)
-    print(vars(model))
+
+    infinity = solver.infinity()
+
+    x = solver.IntVar(0.0, infinity, "x")
+    z = solver.NumVar(0.0, infinity, "z")
+
+    solver.Maximize(3 * x + 2 * z)
     status = solver.Solve()
 
     if status == pywraplp.Solver.OPTIMAL:
         print("\n\nSolution:")
         print("Objective value =", solver.Objective().Value())
-        # print("x =", x.solution_value())
-        # print("z =", z.solution_value())
-        # print("c1 dual =", c1.dual_value())
-        # print("c2 dual =", c2.dual_value())
-    else:
-        print("The problem does not have an optimal solution.")
-
-
-# # Define new variables and constraints
-# x = solver.NumVar(0.0, solver.infinity(), "x")
-# y = solver.NumVar(0.0, solver.infinity(), "y")
-# c1 = solver.Add(x + y <= 10)
-# c2 = solver.Add(2 * x + y >= 8)
-
-# # Set the objective function
-# solver.Maximize(3 * x + 4 * y)
-
-# # Solve the problem
-# status = solver.Solve()
-
-# # Print the solution
-# if status == pywraplp.Solver.OPTIMAL:
-#     print("Solution:")
-#     print("Objective value =", solver.Objective().Value())
-#     print("x =", x.solution_value())
-#     print("y =", y.solution_value())
-#     print("c1 dual =", c1.dual_value())
-#     print("c2 dual =", c2.dual_value())
-# else:
-#     print("The problem does not have an optimal solution.")
